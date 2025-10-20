b21a08b715ac90ccb3c1d7f7144d38a5d07e73b1
Your Name <you@example.com>
2025-10-20 19:46:02 -0300

    alterando o código

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 
 //criando a rota inicial
 app.get("/", function(req,res){
-    res.send("<h1>Bem-vindo ao CineExpress!</h1>");
+    res.send("<h1>Bem-vindo ao Cinepress!</h1>");
 })
 
 //rota do cadastro de produtos
